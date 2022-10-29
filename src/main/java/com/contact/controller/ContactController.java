8212659f12a1df123ba96621a27b40e5951bbcee
bdbenu <bdbenu18@gmail.com>
2022-10-30 00:40:30 +0530

    This is the initial commit

---
+++ src/main/java/com/contact/controller/ContactController.java
@@ -0,0 +1,17 @@
+package com.contact.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/contact")
+public class ContactController {
+
+	@GetMapping(value = { "/", "/home" })
+	public String loadHomePage() {
+
+		return "home";
+	}
+
+}
